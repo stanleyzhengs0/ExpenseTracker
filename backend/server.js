3c889db98d80f3f879006e8054afc5c3527a0708
Stanley Zheng <stanleyzhengs@gmail.com>
2025-05-06 14:30:15 -0400

    Complete: Authorization APIs

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,11 @@ const authRoutes = require("./routes/authRoutes")
 
 const app = express();
 
+
+// Enable Cross-Origin Resource Sharing (CORS) for the Express app.
+// This allows the frontend (at CLIENT_URL or any origin if CLIENT_URL is not defined) to make requests to this backend.
+// It explicitly allows GET, POST, PUT, and DELETE methods,
+// and ensures that requests with "Content-Type" and "Authorization" headers are accepted.
 app.use(
     cors({
         origin : process.env.CLIENT_URL || "*",
@@ -21,6 +26,9 @@ app.use(express.json());
 connectDB();
 
 app.use("/api/v1/auth", authRoutes)
+
+// Serve uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`))
